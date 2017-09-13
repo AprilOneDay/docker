383adc8ff039af0d85207def9024df6473043084
四月 <dancefun@163.com>
2017-09-13 13:48:16 +0800

    158

--- src/denha/framework/Controller.php
+++ src/denha/framework/Controller.php
@@ -69,7 +69,6 @@ class Controller
             //处理视图模板
             $template = new Template($path);
             $template->getContent();
-            var_dump($template);
             include $template->loadPath;
         }
     }
