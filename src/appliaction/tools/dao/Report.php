0b9976643a47c4c74a85814b89862180f3baf0ab
四月 <dancefun@163.com>
2017-09-29 10:14:46 +0800

    186

--- src/appliaction/tools/dao/Report.php
+++ src/appliaction/tools/dao/Report.php
@@ -14,7 +14,7 @@ class Report
         $map['goods_id'] = $goodsId;
         $map['ip']       = getIP();
 
-        $id = table('ReportLog')->where($map)->field('id')->find('one');
+        $id = table('ReportLog')->where($map)->field('id')->find();
         if ($id) {
             return array('status' => false, 'msg' => '感谢您的举报！！！');
         }
