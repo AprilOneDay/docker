d9a056743c1b8f38c7f300ffeb923e63e9637bf9
四月 <dancefun@163.com>
2017-10-10 20:09:39 +0800

    222

--- mysql/shell/install_data.sh
+++ mysql/shell/install_data.sh
@@ -2,6 +2,7 @@
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
 source $WORK_PATH/$FILE_1;
 use  mysql;
-update  user  set  authentication_string  =  password('siyue1q2w3e4r') where  user='siyue';
+update  user  set  authentication_string  =  password('siyue1q2w3e4r') where  user='root';
 update  user  set  user  =  'siyue' where  user='root';
 EOF
+service mysql restart;
