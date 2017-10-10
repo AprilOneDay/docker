005c2376c9e38474a20908d91782331e871ac66e
四月 <dancefun@163.com>
2017-10-10 22:42:20 +0800

    mysql主从

--- mysql.slave1/shell/install_data.sh
+++ mysql.slave1/shell/install_data.sh
@@ -4,6 +4,5 @@ source $WORK_PATH/$FILE_1;
 use  mysql;
 update  user  set  authentication_string  =  password('siyue1q2w3e4r') where  user='root';
 update  user  set  user  =  'siyue' where  user='root';
-GRANT REPLICATION SLAVE ON *.* TO 'siyuemaster'@'%' IDENTIFIED BY 'cheng6251';
 EOF
 service mysql restart;
